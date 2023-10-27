71fa6b2341b5cfa2e1d1ace66751ca83bc6a5dcc
enedamian <142589183+enedamian@users.noreply.github.com>
2023-10-27 19:00:36 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,100 @@
+from flask import Flask, jsonify, request
+import datetime
+from productos import lista_productos as productos
+#creamos una instancia de la clase Flask
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "<h1>Hola!</h1>"
+
+
+
+@app.route("/productos", methods=["GET"])
+def getProductos():
+    return jsonify({"productos":productos}), 200  # 200 es el codigo de exito
+
+@app.route("/productos/<int:id>", methods=["GET"])
+def getProducto(id):
+    productosEncontrados = [producto for producto in productos if producto["id"] == id]
+    if (len(productosEncontrados) > 0):
+        return jsonify({"producto": productosEncontrados[0]}), 200 # 200 es el codigo de exito
+    return jsonify({"message": "Producto no encontrado"}), 404 # 404 es el codigo de error de "not found"
+
+# guardamos un nuevo producto
+@app.route("/productos", methods=["POST"])
+def addProducto():
+    nuevoProducto = {
+        "id" : productos[-1]["id"] + 1,
+        "nombre": request.json["nombre"],
+        "precio": request.json["precio"],
+        "cantidad": request.json["cantidad"],
+        "fecha_actualizacion": datetime.date.today().strftime("%Y-%m-%d") # formato "YYYY-MM-DD" , opcional: %H-%M-%S = HH:MM:SS
+    }
+    productos.append(nuevoProducto)
+    return jsonify({"message": "Producto agregado exitosamente", "productos": productos}), 201 # 201 es el codigo de exito de "created"
+
+
+# actualización de producto
+@app.route("/productos/<int:id>", methods=["PUT"])
+def updateProducto(id):
+    productosEncontrados = [producto for producto in productos if producto["id"] == id]
+    if (len(productosEncontrados) > 0):
+        productosEncontrados[0]["nombre"] = request.json["nombre"]
+        productosEncontrados[0]["precio"] = request.json["precio"]
+        productosEncontrados[0]["cantidad"] = request.json["cantidad"]
+        productosEncontrados[0]["fecha_actualizacion"] = datetime.date.today().strftime("%Y-%m-%d")
+        return jsonify({"message": "Producto actualizado exitosamente", "producto": productosEncontrados[0], "productos": productos}), 200
+    else:
+        return jsonify({"message": "Producto no encontrado"}), 404
+
+
+# eliminación de productos:
+@app.route("/productos/<int:id>", methods=["DELETE"])
+def deleteProducto(id):
+    # en el siguiente list comprehension, como no estamos modificando los elementos, la nueva lista se genera como referencia a la lista original
+    productosEncontrados = [producto for producto in productos if producto["id"] == id]
+    if (len(productosEncontrados) > 0):
+        productos.remove(productosEncontrados[0])
+        return jsonify({"message": "Producto eliminado exitosamente", "productos": productos}), 200
+    else:
+        return jsonify({"message": "Producto no encontrado"}), 404
+
+
+# el siguiente IF nos permite ejecutar la aplicacion solamente si este archivo es ejecutado como archivo principal
+# es decir, si este archivo es importado desde otro archivo, el servidor no se va a ejecutar
+if __name__=="__main__":
+    app.run(debug=True)
+
+# Ejemplos de uso de datetime:
+
+# Fecha actual
+hoy = datetime.date.today()
+
+# Fecha de nacimiento
+fecha_nacimiento = datetime.date(2000, 9, 30)
+
+# Diferencia entre ambas fechas
+diferencia = hoy - fecha_nacimiento
+
+# Diferencia en días
+# print(diferencia.days)
+
+# fecha y hora actual
+fecha_hora_actual = datetime.datetime.now()
+
+año = fecha_hora_actual.year
+mes = fecha_hora_actual.month
+día = fecha_hora_actual.day
+hora = fecha_hora_actual.hour
+minuto = fecha_hora_actual.minute
+segundo = fecha_hora_actual.second
+
+# fecha y hora determinados
+fecha_y_hora_nacimiento = datetime.datetime(2000, 9, 26, 10, 30, 0)
+
+# convertir string a datetime
+fecha_str = "2021-09-30"
+fecha = datetime.datetime.strptime(fecha_str, "%Y-%m-%d")
+    
---
+++ productos.py
@@ -0,0 +1,7 @@
+# simulamos datos de una base de datos
+lista_productos = [{"id": 1, "nombre": "laptop", "precio": 800, "cantidad": 4, "fecha_actualizacion": "2023-09-01"},
+                    {"id": 8, "nombre": "mouse", "precio": 40, "cantidad": 10, "fecha_actualizacion": "2023-10-21"},
+                    {"id": 2, "nombre": "monitor", "precio": 400, "cantidad": 3, "fecha_actualizacion": "2023-09-29"},
+                    {"id": 5, "nombre": "teclado", "precio": 80, "cantidad": 5, "fecha_actualizacion": "2023-10-01"},
+                    {"id": 10, "nombre": "parlantes", "precio": 100, "cantidad": 2, "fecha_actualizacion": "2023-10-25"},
+                    {"id": 7, "nombre": "impresora", "precio": 200, "cantidad": 1, "fecha_actualizacion": "2023-09-28"}]
